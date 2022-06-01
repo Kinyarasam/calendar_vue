fe0ee416697591cc5620b3f7c6d603e0ace82ea0
kinyarasam <samkinyara2000@gmail.com>
2022-06-01 19:11:04 +0300

    set to current Date

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,72 +1,51 @@
 <template>
-    <div class="bg-gray-100 min-h-screen flex justify-center items-center font-mono">
-        <div class="bg-white p-8 h-[100%] rounded shadow">
-            <div class="flex border-b py-2">
-                <div> <span class="material-icons">face</span></div>
-                <div>25 January, 2022</div>
-                <div></div> 
+    <div class="bg-gray-200 min-h-screen text-xs flex justify-center items-center font-mono">
+        <div class="bg-white p-2 h-[450px] md:w-[500px] lg:w-[800px] w-72  rounded-xl shadow-md">
+            <div class="flex justify-center">
+                <div>
+                    <div class="flex justify-center items-center md:text-5xl text-3xl text-teal-800 uppercase font-sans font-bold py-2 ">
+                        {{ WeekDaysFullNames[currentDay] }}
+                    </div>
+                    <div class="flex justify-center items-center shadow-inner rounded-xl text-slate-700">
+                        <div class="flex flex-col px-1 items-center">
+                            <span class="material-icons hover:bg-rose-500 text-rose-500 bg-gray-100 hover:text-white hover:shadow cursor-pointer w-6 h-6 flex items-center justify-center rounded-full">arrow_drop_up</span>
+                            <span class="text-xl font-semibold py-1.5">
+                                {{ currentDate.date }}
+                            </span>
+                            <span class="material-icons hover:bg-rose-500 text-rose-500 bg-gray-100 hover:text-white hover:shadow cursor-pointer w-6 h-6 flex items-center justify-center rounded-full">arrow_drop_down</span>
+                        </div>
+                        <div class="flex flex-col px-1 items-center">
+                            <span class="material-icons hover:bg-rose-500 text-rose-500 bg-gray-100 hover:text-white hover:shadow cursor-pointer w-6 h-6 flex items-center justify-center rounded-full">arrow_drop_up</span>
+                            <span class="text-xl font-semibold py-1.5">
+                                {{ month[currentDate.month] }}
+                            </span>
+                            <span class="material-icons hover:bg-rose-500 text-rose-500 bg-gray-100 hover:text-white hover:shadow cursor-pointer w-6 h-6 flex items-center justify-center rounded-full">arrow_drop_down</span>
+                        </div>
+                        <div class="flex flex-col px-1 items-center">
+                            <span class="material-icons hover:bg-rose-500 text-rose-500 bg-gray-100 hover:text-white hover:shadow cursor-pointer w-6 h-6 flex items-center justify-center rounded-full">arrow_drop_up</span>
+                            <span class="text-xl font-semibold py-1.5">
+                                {{ currentDate.year }}
+                            </span>
+                            <span class="material-icons hover:bg-rose-500 text-rose-500 bg-gray-100 hover:text-white hover:shadow cursor-pointer w-6 h-6 flex items-center justify-center rounded-full">arrow_drop_down</span>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
-            <table class="shadow-inner my-4">
-                <thead>
-                    <tr class="">
-                        <th class="px-2 py-1 shadow border">Sunday</th>
-                        <th class="px-2 py-1 shadow border">Monday</th>
-                        <th class="px-2 py-1 shadow border">Tuesday</th>
-                        <th class="px-2 py-1 shadow border">Wednesday</th>
-                        <th class="px-2 py-1 shadow border">Thursday</th>
-                        <th class="px-2 py-1 shadow border">Friday</th>
-                        <th class="px-2 py-1 shadow border">Saturday</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="border">1</td>
-                        <td class="border">2</td>
-                        <td class="border">3</td>
-                        <td class="border">4</td>
-                        <td class="border">5</td>
-                        <td class="border">6</td>
-                        <td class="border">7</td>
-                    </tr>
-                    <tr>
-                        <td class="border">8</td>
-                        <td class="border">9</td>
-                        <td class="border">10</td>
-                        <td class="border">11</td>
-                        <td class="border">12</td>
-                        <td class="border">13</td>
-                        <td class="border">14</td>
-                    </tr>
-                    <tr>
-                        <td class="border">15</td>
-                        <td class="border">16</td>
-                        <td class="border">17</td>
-                        <td class="border">18</td>
-                        <td class="border">19</td>
-                        <td class="border">20</td>
-                        <td class="border">21</td>
-                    </tr>
-                    <tr>
-                        <td class="border">22</td>
-                        <td class="border">23</td>
-                        <td class="border">24</td>
-                        <td class="border">25</td>
-                        <td class="border">26</td>
-                        <td class="border">27</td>
-                        <td class="border">28</td>
-                    </tr>
-                    <tr>
-                        <td class="border">29</td>
-                        <td class="border">30</td>
-                        <td class="border">31</td>
-                        <td class="border">1</td>
-                        <td class="border">2</td>
-                        <td class="border">3</td>
-                        <td class="border">4</td>
-                    </tr>
-
-                </tbody>
-            </table>
+            
+            <!-- <div class="flex border-b py-2 items-center">
+                <button class="border rounded-full h-8 w-8 flex items-center justify-center"> <span class="material-icons">chevron_left</span></button>
+                <div>
+                    div
+                    <span>June </span> 
+                    <span>1</span>,
+                    <span>2022</span>
+                </div>
+                <button  class="border rounded-full h-8 w-8 flex items-center justify-center"> <span class="material-icons">chevron_right</span></button>
+            </div>
+            <div class="shadow-inner my-4">
+                
+            </div> -->
         </div>
     </div>
 </template>
@@ -75,27 +54,57 @@
 export default {
     data() {
         return {
-            DaysOfTheWeek: [
-                {title: "Sunday"},
-                {title: "Sunday"},
-                {title: "Sunday"},
-                {title: "Sunday"},
-
-            ]
+            WeekDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+            WeekDaysFullNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            currentDate: {
+                date: 0,
+                month: 0,
+                year: 0
+            },
+            DayOfTheWeek: []
+        }
+    },
+    computed: {
+        currentDay() {
+            return new Date(
+                this.currentDate.year, this.currentDate.month, this.currentDate.date
+            ).getDay()
         }
     },
     methods: {
-        getWeekdays() {
+        getCurrentDate() {
+            let today = new Date()
+
+            this.currentDate.date = today.getDate()
+            this.currentDate.month = today.getMonth()
+            this.currentDate.year = today.getFullYear()
+        },
+        getWeekday() {
             const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
             const d = new Date()
             let day = weekday[d.getDay()]
 
             // this.DaysOfTheWeek = day
-            // console.log(this.DaysOfTheWeek)
+            console.log(day)
+        },
+        getyear() {
+
+            // const year = new Date().getFullYear(),
+            // console.log(year)
+        },
+        getNumberOfDays() {
+            const year = new Date().getFullYear()
+            const month = new Date().getMonth()
+
+            const NumDays = new Date(year, (month + 1), 0).getDate();
+            console.log(`Month ${month + 1} has ${NumDays} days`)
         }
     },
     mounted() {
-        this.getWeekdays()       
+        this.getCurrentDate()
+        this.getWeekday() 
+        this.getNumberOfDays()      
     },
 }
 </script>
