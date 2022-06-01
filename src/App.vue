2a8c2bee4a94250f20871125363cac7a33b99c11
kinyarasam <samkinyara2000@gmail.com>
2022-06-01 11:00:02 +0300

    Tailwind Setup and configuration

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Calendar from './views/Calendar.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <Calendar />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/views/Calendar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-teal-500 min-h-screen">
+        Calendar
+    </div>
+</template>
